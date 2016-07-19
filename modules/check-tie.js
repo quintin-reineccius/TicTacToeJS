894cda8f5bade5b5a8c9777d56ec6eaa96625511
rampage1213 <quintinreineccius@gmail.com>
2016-07-19 13:55:49 -0400

    checkTie logic improved

--- modules/check-tie.js
+++ modules/check-tie.js
@@ -1,23 +1,38 @@
 import { buttons, wins, winner, board } from './config';
 
 export default function checkTie(){
-  //needs cleaning up
+  let column = 0
+  let row = 0
+
+  for (column; column < 9; column++) {
+    if(column === 3 || column === 6) {
+      row++
+    }
+    column++
+  }
+
   var isThereATieTop = buttons
-    .map(button => button[0].disabled ? true : null)
+    .map(button => button[row].disabled ? true : null)
     .filter(i => i)
     .length === 3
 
-  var isThereATieMiddle = buttons
-    .map(button => button[1].disabled ? true : null)
-    .filter(i => i)
-    .length === 3
+  //needs cleaning up
+  // var isThereATieTop = buttons
+  //   .map(button => button[0].disabled ? true : null)
+  //   .filter(i => i)
+  //   .length === 3
+  //
+  // var isThereATieMiddle = buttons
+  //   .map(button => button[1].disabled ? true : null)
+  //   .filter(i => i)
+  //   .length === 3
+  //
+  // var isThereATieBottom = buttons
+  //   .map(button => button[2].disabled ? true : null)
+  //   .filter(i => i)
+  //   .length === 3
 
-  var isThereATieBottom = buttons
-    .map(button => button[2].disabled ? true : null)
-    .filter(i => i)
-    .length === 3
-
-  if (isThereATieTop && isThereATieMiddle && isThereATieBottom){
+  if (isThereATieTop){
     wins['ties'] += 1;
     document.getElementById('ties-holder').innerHTML = wins['ties'];
     winner.innerHTML = 'Tie!';

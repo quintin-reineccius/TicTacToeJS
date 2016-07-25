c2299ae719eddd97f1c37d1e447720725750d158
rampage1213 <quintinreineccius@gmail.com>
2016-07-25 19:08:44 -0400

    fix targeting error

--- modules/check-move/index.js
+++ modules/check-move/index.js
@@ -1,7 +1,7 @@
 import currentRow from './current-row'
 import currentColumn from './current-column'
 import currentDiagonalLeft from './current-diagonal-left'
-import currentDiagonalRight from './current-diagonal-Right'
+import currentDiagonalRight from './current-diagonal-right'
 import fullBoardCheck from './full-board-check'
 import updateHTML from '../update-html'
 
